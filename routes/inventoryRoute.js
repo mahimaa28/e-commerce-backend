c2264a160f24ba51eb1d06d908b0c0b106db2dba
mahimaa28 <111148542+mahimaa28@users.noreply.github.com>
2024-02-01 01:30:41 +0530

    get product details and get inventory deatils

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { createInventory, getInventory } = require("../controllers/inventoryController");
+const { createInventory, getInventory, deleteInventory, updateInventory, getInventoryDetails } = require("../controllers/inventoryController");
 const router = express.Router();
 
 router.route("/createInventory").post(createInventory);
 router.route("/getInventory").get(getInventory);
+router.route("/updateInventory/:id").put(updateInventory);
+router.route("/deleteInventory/:id").delete(deleteInventory);
+router.route("/getInventoryDetails/:id").get(getInventoryDetails);
 
 module.exports = router;
