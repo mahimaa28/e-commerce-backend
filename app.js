55debd907709f7400c59c13d34814f85a2f3a44e
mahimaa28 <111148542+mahimaa28@users.noreply.github.com>
2024-04-12 13:39:38 +0530

    payment

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const seller = require("./routes/sellerRoute");
 const admin = require("./routes/superAdminRoute");
 const cart = require("./routes/cartRoute");
 const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
 //Super Admin is referred in this app as ADMIN, don't get confused.
 
 // routes ---------------------
@@ -34,5 +35,6 @@ app.use("/api/v1/seller", seller);
 app.use("/api/v1/admin", admin);
 app.use("/api/v1/cart", cart);
 app.use("/api/v1/order", order);
+app.use("/api/v1/payment", payment);
 
 module.exports = app;
---
+++ routes/paymentRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { processPayment } = require("../controllers/paymentController");
+const { isAuthenticatedUser } = require("../middlewares/auth");
+const { route } = require("./orderRoute");
+const router = express.Router();
+
+router.route("/process-payment").post(isAuthenticatedUser, processPayment);
+
+module.exports = router;
