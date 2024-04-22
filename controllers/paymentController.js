6c95c6768527863cf289ef8aad791b52c336f7d2
mahimaa28 <111148542+mahimaa28@users.noreply.github.com>
2024-04-22 12:17:46 +0530

    commented p

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,52 +1,52 @@
-const stripe = require("stripe")(
-  sk_test_51P2fovSBuekwIuDIGkY1waTkY9b70NqmMRFVS7cnEV3AkaWfPGOXYjUpDvhznDZhudULqL19pgTvSr43twTwcgEf00jcp14hI4
-);
-const Order = require("../models/orderModel");
+// const stripe = require("stripe")(
+//   sk_test_51P2fovSBuekwIuDIGkY1waTkY9b70NqmMRFVS7cnEV3AkaWfPGOXYjUpDvhznDZhudULqL19pgTvSr43twTwcgEf00jcp14hI4
+// );
+// const Order = require("../models/orderModel");
 
-exports.processPayment = async (req, res, next) => {
-  try {
-    const { userId } = req.body;
+// exports.processPayment = async (req, res, next) => {
+//   try {
+//     const { userId } = req.body;
 
-    // Find all orders for the specified user
-    const orders = await Order.find({ userId });
+//     // Find all orders for the specified user
+//     const orders = await Order.find({ userId });
 
-    // Calculate the total amount to charge
-    let totalAmount = 0;
-    let lineItems = [];
-    for (const order of orders) {
-      totalAmount += order.totalPrice; // Assuming totalPrice is stored in the Order schema
+//     // Calculate the total amount to charge
+//     let totalAmount = 0;
+//     let lineItems = [];
+//     for (const order of orders) {
+//       totalAmount += order.totalPrice; // Assuming totalPrice is stored in the Order schema
 
-      // Iterate over products in the order and construct line items
-      for (const product of order.products) {
-        lineItems.push({
-          price_data: {
-            currency: "usd", // Currency of the price
-            product_data: {
-              name: product.name, // Name of the product
-              description: product.description, // Description of the product
-            },
-            unit_amount: product.price * 100, // Price in the smallest currency unit (e.g., cents)
-          },
-          quantity: product.quantity, // Quantity of the product
-        });
-      }
-    }
+//       // Iterate over products in the order and construct line items
+//       for (const product of order.products) {
+//         lineItems.push({
+//           price_data: {
+//             currency: "usd", // Currency of the price
+//             product_data: {
+//               name: product.name, // Name of the product
+//               description: product.description, // Description of the product
+//             },
+//             unit_amount: product.price * 100, // Price in the smallest currency unit (e.g., cents)
+//           },
+//           quantity: product.quantity, // Quantity of the product
+//         });
+//       }
+//     }
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment", //for one time payment, use subscription for subscription
-      line_items: lineItems,
-      success_url: "http://localhost:3000/orderPlaced", // URL to redirect to after successful payment
-      cancel_url: "http://localhost:3000/cancel-order", // URL to redirect to if the user cancels the payment
-    });
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ["card"],
+//       mode: "payment", //for one time payment, use subscription for subscription
+//       line_items: lineItems,
+//       success_url: "http://localhost:3000/orderPlaced", // URL to redirect to after successful payment
+//       cancel_url: "http://localhost:3000/cancel-order", // URL to redirect to if the user cancels the payment
+//     });
 
-    res.status(200).json({
-      success: true,
-      message: "Payment processed successfully",
-      url: session.url,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
+//     res.status(200).json({
+//       success: true,
+//       message: "Payment processed successfully",
+//       url: session.url,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({ success: false, message: error.message });
+//   }
+// };
--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -4,6 +4,6 @@ const { isAuthenticatedUser } = require("../middlewares/auth");
 const { route } = require("./orderRoute");
 const router = express.Router();
 
-router.route("/process-payment").post(isAuthenticatedUser, processPayment);
+// router.route("/process-payment").post(isAuthenticatedUser, processPayment);
 
 module.exports = router;
